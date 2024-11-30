95609b05e8832fa2ecf21f1436ce2aa3494edd42
hunte2 <143753564+hunte2@users.noreply.github.com>
2024-11-29 16:56:11 -0800

    Add files via upload

--- lab7/task2.cpp
+++ lab7/task2.cpp
@@ -12,6 +12,40 @@ int main() {
 	std::string line;
 	std::vector<double> p, h, t;
 	//follow the lab instructions to generate task1.png, task2a.png and task2b.png
+
+	getline(std::cin, line);
+	getline(std::cin, line);
+	getline(std::cin, line);
+	getline(std::cin, line);
+
+
+
+	while(getline(ifile, line)){
+		std::stringstream ss(line);
+		double pp, hh, tt;
+		ss >> pp >> hh >> tt;
+		//getline(std::cin, line);
+		p.push_back(pp);
+		h.push_back(hh);
+		t.push_back(tt);
+	}
+
+	double t_min = *std::min_element(t.begin(), t.end());
+	double t_max = *std::max_element(t.begin(), t.end());
+
+	auto fig = figure(true);
+	plot(h, p, "-b")->line_width(2);
+    xlabel("Height (meters)");
+    ylabel("Pressure (hPa)");
+	grid(on);
+	//show();
+	//save("task2a.png");
+	hold(on);
+	plot(h,t, "-rx")->use_y2(true);
+	y2lim({t_min, t_max});
+	y2label("Temperature (Celsius)");
+	save("task2b.png");
+
 	
 	
 	return 0;
